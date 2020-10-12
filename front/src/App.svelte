3b0ab58d6f47f87474c96cd3d935813849599d55
chaechaep <codjs0910@naver.com>
2020-10-12 17:46:52 +0900

    first commit

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,10 +1,60 @@
 <script>
+	import {onMount} from "svelte";
+
 	export let name;
+
+	if ('serviceWorker' in navigator) {
+		window.addEventListener('load', ()=> {
+			navigator.serviceWorker.register('./sw.js').then(registration=> {
+				console.log('ServiceWorker registration successful with scope: ', registration.scope);
+			}, err => {
+				console.log('ServiceWorker registration failed : ', err);
+			})
+		})
+	}
+
+	let deferredPrompt;
+	onMount(()=>{
+
+		window.addEventListener('beforeinstallprompt', function (e){
+			console.log('beforeinstallprompt Event fired');
+			e.preventDefault();
+
+			deferredPrompt = e;
+
+			return false;
+		})
+	})
+
+	let btnEx;
+	$: if (btnEx) {
+		btnEx.addEventListener('click', function (){
+			console.log('click!');
+			console.log(deferredPrompt);
+			if (deferredPrompt !== undefined) {
+				deferredPrompt.prompt();
+
+				deferredPrompt.userChoice.then(function (choiceResult) {
+					console.log(choiceResult.outcome);
+
+					if (choiceResult.outcome == 'dismissed') {
+						console.log('User cancelled home screen install');
+					} else {
+						console.log('User added to home screen');
+					}
+
+					deferredPrompt = null;
+				})
+			}
+		})
+	}
+
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div bind:this={btnEx} style="width: 100px; height: 100px; border: 1px solid gray;">버튼</div>
 </main>
 
 <style>
